9f6e01217f4a1825e798cef369300cece4b92eb0
ShivaniVarmaG <59469222+ShivaniVarmaG@users.noreply.github.com>
2020-06-09 18:08:56 +0530

    Update app.component.ts

--- forms-reactive-final/src/app/app.component.ts
+++ forms-reactive-final/src/app/app.component.ts
@@ -18,7 +18,8 @@ export class AppComponent implements OnInit {
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
         'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
-        'email': new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails)
+        'email': new FormControl(null, [Validators.required, Validators.email])
+//         , this.forbiddenEmails)
       }),
       'gender': new FormControl('male'),
       'hobbies': new FormArray([])
@@ -54,23 +55,23 @@ export class AppComponent implements OnInit {
     (<FormArray>this.signupForm.get('hobbies')).push(control);
   }
 
-  forbiddenNames(control: FormControl): {[s: string]: boolean} {
+  forbiddenNames(control: FormControl): {[key: string]: boolean} {
     if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
       return {'nameIsForbidden': true};
     }
     return null;
   }
 
-  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
-    const promise = new Promise<any>((resolve, reject) => {
-      setTimeout(() => {
-        if (control.value === 'test@test.com') {
-          resolve({'emailIsForbidden': true});
-        } else {
-          resolve(null);
-        }
-      }, 1500);
-    });
-    return promise;
-  }
+//   forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+//     const promise = new Promise<any>((resolve, reject) => {
+//       setTimeout(() => {
+//         if (control.value === 'test@test.com') {
+//           resolve({'emailIsForbidden': true});
+//         } else {
+//           resolve(null);
+//         }
+//       }, 1500);
+//     });
+//     return promise;
+//   }
 }
